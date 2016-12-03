1a226774d31f19f10509e058a704d6b17e069588
Jamasen Rodriguez <jamasenr@gmail.com>
2016-12-03 14:26:55 -0800

    added the bubble navcss and the bubble nav to the settings html

--- public/scripts/welcome.js
+++ public/scripts/welcome.js
@@ -1,28 +1,43 @@
-var userDetails = {
-  name: "user",
-  email: "email",
-  role: "role"
-}
+
+var 
+  name,
+  email,
+  role;
 
 $(document).ready(function(){
 
-  $('#form-1-btn').click(form1Transition);
+
+  $('#form-1-btn').click(form1capture);
   $('#form-2-btn').click(form2Transition);
 
   $('#form-2-back').click(form2BackTransition);
   $('#form-3-back').click(form3BackTransition);
 });
 
+function form1capture(){
+  if(document.getElementById('q1-option1').checked){
+      role = $("#q1-option1").val();
+      form1Transition();
+    }else if(document.getElementById('q1-option2').checked){
+      role = $("#q1-option2").val();
+      form1Transition();
+    }else if(document.getElementById('q1-option3').checked){
+      role = $("#q1-option3").val();
+      form1Transition();
+    }else if(document.getElementById('q1-option4').checked){
+      role = $("#q1-option4").val();
+      form1Transition();
+    }
+}
 function form1Transition (){
+    $('#form-1').addClass('form-hide');
+    $('#form-2').removeClass('form-hide');
 
-  $('#form-1').addClass('form-hide');
-  $('#form-2').removeClass('form-hide');
+    $('#form-1-circle').addClass('fa-circle-o');
+    $('#form-1-circle').removeClass('fa-circle');
 
-  $('#form-1-circle').addClass('fa-circle-o');
-  $('#form-1-circle').removeClass('fa-circle');
-
-  $('#form-2-circle').addClass('fa-circle');
-  $('#form-2-circle').removeClass('fa-circle-o');
+    $('#form-2-circle').addClass('fa-circle');
+    $('#form-2-circle').removeClass('fa-circle-o');
 }
 
 function form2Transition (){
