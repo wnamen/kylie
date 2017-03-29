28c1cc3592d15ef6af15d622601c419b2dbb55ed
wnamen <williamjosephnamen@gmail.com>
2017-03-29 10:39:11 -0700

    added invoice form and animations

--- client/scripts/signup.js
+++ client/scripts/signup.js
@@ -2,4 +2,28 @@ $(document).ready(function() {
   $('.scrollspy').scrollSpy();
   $('.modal').modal();
   $('select').material_select();
+
+  $('.payment-card').click(handlePaymentSelection)
 });
+
+function handlePaymentSelection() {
+
+  var selection = $(this).data('payment-option');
+  var selectedID = '#' + selection + '-form';
+
+  if (selection === 'card') {
+    $('#payment-selector').addClass('animated fadeOutRight');
+    setTimeout(function() {
+      $('#payment-selector').addClass('hide');
+      $(selectedID).addClass('show');
+      $(selectedID).addClass('animated fadeInLeft');
+    }, 500)
+  } else if (selection === 'invoice') {
+    $('#payment-selector').addClass('animated fadeOutLeft');
+    setTimeout(function() {
+      $('#payment-selector').addClass('hide');
+      $(selectedID).addClass('animated fadeInRight');
+      $(selectedID).addClass('show');
+    }, 500)
+  }
+}
