906240431bf257d0f8d650322f80b700efca56c6
wnamen <williamjosephnamen@gmail.com>
2017-03-31 17:39:41 -0700

    finished general html and styling and created modal with dynamic building

--- client/scripts/dashboard.js
+++ client/scripts/dashboard.js
@@ -1,8 +1,11 @@
 var currentView = 'settings';
+var currentCard = 'admin'
 
 $(document).ready(function() {
 
   $('.nav-options i').click(handleViewChange);
+  $('#controller-cards').on('click', '.controller-card', handleCardSelection);
+  $('#add-role-modal form .btn').click(handleAddNewRole);
 
   $('.modal').modal();
   $('select').material_select();
@@ -21,4 +24,44 @@ function handleViewChange() {
     $(currentId).toggleClass('active');
   }
   currentView = newView;
+}
+
+function handleCardSelection() {
+  var newCard = $(this).data('role');
+
+  if (newCard === currentCard) {
+    return
+  } else {
+    var newId = '#' + newCard + '-card';
+    var currentId = '#' + currentCard + '-card';
+
+    $(newId).toggleClass('active-card');
+    $(currentId).toggleClass('active-card');
+  }
+  currentCard = newCard;
+}
+
+function handleAddNewRole() {
+  var newRole = $('#add-role-modal form').serializeArray();
+
+  createNewRole(newRole)
+}
+
+function createNewRole(data) {
+  var roleName = findDataPoint(data, 'role-name');
+  var roleDescription = findDataPoint(data, 'role-description');
+
+  var $newRoleCard = $("<div id='" + roleName + "-card' data-role='" + roleName + "' class='controller-card'><p class='controller-title'>" + roleName + "</p><p class='controller-description'>A <span class='description-title'>" + roleName + "</span> can " + roleDescription + "</p></div>")
+  $('#controller-cards').append($newRoleCard);
+}
+
+function findDataPoint(data, point) {
+  var locatedPoint;
+  data.forEach(function(datapoint) {
+    if (datapoint.name === point) {
+      locatedPoint = datapoint.value;
+    }
+  });
+
+  return locatedPoint;
 }
